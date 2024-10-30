6f9ee746bf1cb6655042ec1878ed3b5ddbc8b064
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-10-30 14:40:26 +0200

    add buttons

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,20 +1,42 @@
 import {useState, useEffect} from 'react';
 
 const Timer = () => {
-    const [time, setTime] = useState(0);
+    const [time, setTime] = useState (0);
+    const [run , setRun] = useState (false);
     useEffect(() => {
         console.log('create effect');
-        
-        return () => {
-            console.log('clear effect ');
-            
-        };
-    }, []);
+        if (run) {
+            console.log('create setInterval');
+            const idInterval = setInterval(()=>{
+                setTime((prevTime) => prevTime + 1);
+            }, 1000);
+            return () => {
+                console.log('clear effect ');
+                clearInterval(idInterval);
+            };
+        }   
+     
+    }, [run]);
+    const startTimer = () => {setRun(true)}
+    const stopTimer = () => {setRun(false)}
+    const resetTimer = () => {setRun(false);setTime(0);}
+
     return (
         <article>
             <h2>
                 Timer: {time} seconds 
             </h2>
+            <div>
+                <button onClick ={startTimer}> 
+                    start
+                    </button>
+                <button onClick ={stopTimer}>
+                    stop
+                    </button>
+                <button onClick ={resetTimer}>
+                    reset
+                    </button>
+            </div>
         </article>
     );
 }
